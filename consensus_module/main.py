e0f5a599b3e65e11978ebff8cbad5f37edab1d7b
luana-garcia <luanaqg@estudante.ufscar.br>
2024-09-27 14:58:33 +0200

    Adding datasets of experiment

--- consensus_module/main.py
+++ consensus_module/main.py
@@ -5,17 +5,17 @@ absolute_path = os.path.dirname(__file__)
 
 datasets_path = os.path.join(absolute_path, "../datasets/")
 
-# c1_85_95_1k_samples = ConsensusModule(datasets_path+"1k_samples_cluster1_85_95.csv", "id_")
-# c1_85_95_1k_samples.export_top_k_ranking("Cluster1_85_95_1k_Samples", 1)
+c1_85_95_1k_samples = ConsensusModule(datasets_path+"1k_samples_cluster1_85_95.csv", "id_")
+c1_85_95_1k_samples.export_top_k_ranking("Cluster1_85_95_1k_Samples", 1)
 
-# c1_95_1k_samples = ConsensusModule(datasets_path+"1k_samples_cluster1_95.csv", "id_")
-# c1_95_1k_samples.export_top_k_ranking("Cluster1_95_1k_Samples", 1)
+c1_95_1k_samples = ConsensusModule(datasets_path+"1k_samples_cluster1_95.csv", "id_")
+c1_95_1k_samples.export_top_k_ranking("Cluster1_95_1k_Samples", 1)
 
-# c2_85_95_1k_samples = ConsensusModule(datasets_path+"1k_samples_cluster2_85_95.csv", "id_")
-# c2_85_95_1k_samples.export_top_k_ranking("Cluster2_85_95_1k_Samples", 1)
+c2_85_95_1k_samples = ConsensusModule(datasets_path+"1k_samples_cluster2_85_95.csv", "id_")
+c2_85_95_1k_samples.export_top_k_ranking("Cluster2_85_95_1k_Samples", 1)
 
-# c2_95_1k_samples = ConsensusModule(datasets_path+"1k_samples_cluster2_95.csv", "id_")
-# c2_95_1k_samples.export_top_k_ranking("Cluster2_95_1k_Samples", 1)
+c2_95_1k_samples = ConsensusModule(datasets_path+"1k_samples_cluster2_95.csv", "id_")
+c2_95_1k_samples.export_top_k_ranking("Cluster2_95_1k_Samples", 1)
 
 # positive_samples = ConsensusModule(datasets_path+"Amostra-Dataset.csv", "id_")
 # positive_samples.export_top_k_ranking("10_positive_samples")
@@ -23,8 +23,8 @@ datasets_path = os.path.join(absolute_path, "../datasets/")
 # negative_samples = ConsensusModule(datasets_path+"NO_instances.csv", "id")
 # negative_samples.export_top_k_ranking("10_negative_samples", 5, 1)
 
-yes_samples = ConsensusModule(datasets_path+"Yes_Instances_Experiment.csv", "id", "y")
-yes_samples.export_top_k_ranking("yes_experiment", 3, 1)
+# yes_samples = ConsensusModule(datasets_path+"Yes_Instances_Experiment.csv", "id", "y")
+# yes_samples.export_top_k_ranking("yes_experiment", 3, 1)
 
 # no_samples = ConsensusModule(datasets_path+"No_Instances_Experiment.csv", "id", "Y")
 # no_samples.export_top_k_ranking("no_experiment")
